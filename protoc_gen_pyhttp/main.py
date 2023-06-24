75c01fe8da164c0e4ecae29598c5066b0637fa89
strluck <s@strluck.com>
2023-06-24 16:27:09 +0800

    fix: 修复需要构建多个目标文件时只构建最后一个的问题

--- protoc_gen_pyhttp/main.py
+++ protoc_gen_pyhttp/main.py
@@ -9,27 +9,25 @@ if __package__ is None and not hasattr(sys, "frozen"):
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 from protoc_gen_pyhttp import http
 
-__version__ = "1.0.0rc1"
+__version__ = "1.0.0rc2"
 
 
-def main() -> None:
+def main():
     request = plugin.CodeGeneratorRequest.FromString(sys.stdin.buffer.read())
-
-    response = plugin.CodeGeneratorResponse()
-    response.supported_features = plugin.CodeGeneratorResponse.FEATURE_PROTO3_OPTIONAL
 
     pool = DescriptorPool()
     for proto in request.proto_file:
         pool.Add(proto)
 
-    gen = response.file.add()
-
     for proto_file in request.proto_file:
+        response = plugin.CodeGeneratorResponse()
+        response.supported_features = plugin.CodeGeneratorResponse.FEATURE_PROTO3_OPTIONAL
+
+        gen = response.file.add()
         http.generate_file(proto_file, pool, gen)
 
-    sys.stdout.buffer.write(response.SerializeToString())
+        sys.stdout.buffer.write(response.SerializeToString())
 
 
 if __name__ == "__main__":
     main()
-
